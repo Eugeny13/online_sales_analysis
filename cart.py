52b3b07677fdfaa101bdcf8f0f18cc194406dde0
Eugeny13 <neugeny13@yahoo.com>
2025-02-10 15:10:15 -0500

    Add files via upload

--- cart.py
+++ cart.py
@@ -1,15 +1,33 @@
-class Cart:
-    def __init__(self, cart_items=list):
-        self.cart_items = cart_items
+from product import Product
 
-    def add_product(self,product,quantity):
-        if product in self.cart_items:
-            self.cart_items[product] += quantity
+class Cart:
+    def __init__(self):
+        self.cart_items = []
+
+    def add_to_cart(self, product, quantity):
+      
+        if quantity <= product.quantity:
+            self.cart_items.append({"product": product, "quantity": quantity})
+            product.update_quantity(-quantity) 
         else:
-            self.cart_items[product] = quantity
-    def total_spent(self):
-        price = 0.0
-        quantity = 0
-        return sum (price() * quantity for product, quantity in self.cart_items())
-    def display_info(cart_items):
-        return("Cart: {self.cart_items}, total spent: {total_spent}")
+            print(f"Not enough stock for {product.name}. Available: {product.quantity}")
+
+    def calculate_total(self):
+      
+        total = sum(item["product"].price * item["quantity"] for item in self.cart_items)
+        return total
+
+    def display_cart(self):
+      
+        if not self.cart_items:
+            print("Your cart is empty.")
+        else:
+            print("Cart Contents:")
+            for item in self.cart_items:
+                product = item["product"]
+                quantity = item["quantity"]
+                print(f"{product.name} - {quantity} pcs - ${product.price * quantity:.2f}")
+
+
+
+
